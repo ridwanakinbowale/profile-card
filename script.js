f2f34f717f3f91a318e590694f4ab0f3610595b3
Ridwan Akinbowale <akinbowaleridwan87@gmail.com>
2025-07-27 00:37:28 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button =document.querySelector("#btn");
+    button,this.addEventListener("click",function () {
+        alert("UI/UX,& GRAPHICS DESIGn")
+    })
+} )
